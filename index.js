4d01d5bef97ebfff331bd1370a4878930e248ccc
Clement <c.raballand@bimedia.com>
2021-08-03 16:33:55 +0200

    feat: use express json parser for incoming requests

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routes = ['tickets', 'comments'];
 
+app.use(express.json());
+
 typeorm.createConnection()
     .then(connection => {
         Db.setInstance(connection);
