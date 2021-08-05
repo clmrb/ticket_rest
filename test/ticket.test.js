59a6b39fc1dabcbb233dfad28a9bcedf658c9a16
Clement <c.raballand@bimedia.com>
2021-08-05 14:50:06 +0200

    test: added GET /ticket/:id/comments tests and refactored to async instead of callback

--- test/ticket.test.js
+++ test/ticket.test.js
@@ -5,10 +5,12 @@ const server = app.server;
 const ready = app.ready;
 
 const should = chai.should();
+const expect = chai.expect;
 chai.use(chaiHttp);
 
 let connection = null;
 let TicketRepo = null;
+let CommentRepo = null;
 let UserRepo = null;
 
 describe('Ticket', () => {
@@ -19,8 +21,10 @@ describe('Ticket', () => {
 
         connection = require('typeorm').getConnection();
         TicketRepo = connection.getRepository('Ticket');
+        CommentRepo = connection.getRepository('Comment');
         UserRepo = connection.getRepository('User');
 
+        await CommentRepo.delete({});
         await TicketRepo.delete({});
         await UserRepo.delete({});
         testUser = await UserRepo.save({
@@ -29,134 +33,182 @@ describe('Ticket', () => {
         });
     });
 
-    // empty ticket table after each test
-    afterEach(() => TicketRepo.delete({}));
-
     describe('GET /ticket/:id', () => {
         let ticket = null;
 
-        before(async () => {
+        it('it should find a ticket', async () => {
             ticket = await TicketRepo.save({
                 user: testUser,
                 title: 'test',
                 description: 'test'
             });
-        });
 
-        it('it should find a ticket', (done) => {
-            chai.request(server)
+            const res = await chai.request(server)
                 .get(`/ticket/${ticket.id}`)
-                .set('Authorization', 'Bearer test@testdomain.com')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.an('object');
+                .set('Authorization', 'Bearer test@testdomain.com');
 
-                    res.body.title.should.eql('test');
-                    res.body.description.should.eql('test');
-                    res.body.status.should.eql('todo');
-                    done();
-                });
+            res.should.have.status(200);
+            res.body.should.be.an('object');
+
+            res.body.title.should.eql('test');
+            res.body.description.should.eql('test');
+            res.body.status.should.eql('todo');
         });
 
-        it('it should fail to find a ticket (404)', (done) => {
-            chai.request(server)
-                .get(`/ticket/${ticket.id + 1}`)
-                .set('Authorization', 'Bearer test@testdomain.com')
-                .end((err, res) => {
-                    res.should.have.status(404);
-                    res.body.should.be.an('object');
+        it('it should fail to find a ticket (404)', async () => {
+            await CommentRepo.delete({});
+            await TicketRepo.delete({});
 
-                    should.exist(res.body.message);
-                    done();
-                });
+            const res = await chai.request(server)
+                .get(`/ticket/5`)
+                .set('Authorization', 'Bearer test@testdomain.com');
+
+            res.should.have.status(404);
+            res.body.should.be.an('object');
+
+            should.exist(res.body.message);
+        });
+    });
+
+    describe('GET /ticket/:id/comments', () => {
+        let ticketWithComments = null;
+        let ticketNoComments = null;
+
+        it('it should get 2 comments', async () => {
+            await CommentRepo.delete({});
+            await TicketRepo.delete({});
+
+            ticketWithComments = await TicketRepo.save({
+                user: testUser,
+                title: 'test',
+                description: 'test'
+            });
+            ticketNoComments = await TicketRepo.save({
+                user: testUser,
+                title: 'test2',
+                description: 'test2'
+            });
+            await CommentRepo.save([
+                {
+                    text: 'comment 1',
+                    createdAt: new Date(),
+                    ticket: ticketWithComments,
+                    testUser
+                },
+                {
+                    text: 'comment 2',
+                    createdAt: new Date(),
+                    ticket: ticketWithComments,
+                    user: testUser
+                }
+            ]);
+
+            const res = await chai.request(server)
+                .get(`/ticket/${ticketWithComments.id}/comments`)
+                .set('Authorization', 'Bearer test@testdomain.com');
+
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.eql(2);
+
+            res.body[0].text.should.eql('comment 1');
+            res.body[1].text.should.eql('comment 2');
+        });
+
+        it('it should not find ticket (404)', async () => {
+            const res = await chai.request(server)
+                .get(`/ticket/0/comments`)
+                .set('Authorization', 'Bearer test@testdomain.com');
+
+            res.should.have.status(404);
+            res.body.should.be.an('object');
+            should.exist(res.body.message);
+        });
+
+        it('it should find 0 comments', async () => {
+            const res = await chai.request(server)
+                .get(`/ticket/${ticketNoComments.id}/comments`)
+                .set('Authorization', 'Bearer test@testdomain.com');
+
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.eql(0);
         });
     });
 
     describe('POST /ticket', () => {
-        it('it should return 401 (no authorization)', (done) => {
-            chai.request(server)
+        it('it should return 401 (no authorization)', async () => {
+            const res = await chai.request(server)
+                .post('/ticket')
+                .send({
+                    title: 'Ticket test',
+                    description: 'Ticket description'
+                });
+
+            res.should.have.status(401);
+            res.body.should.be.an('object');
+
+            should.exist(res.body.message);
+        });
+
+        it('it should return 401 (invalid authorization format)', async () => {
+            const res = await chai.request(server)
                 .post('/ticket')
                 .send({
                     title: 'Ticket test',
                     description: 'Ticket description'
                 })
-                .end((err, res) => {
-                    res.should.have.status(401);
-                    res.body.should.be.an('object');
+                .set('Authorization', 'test@testdomain.com');
 
-                    should.exist(res.body.message);
-                    done();
-                });
+            res.should.have.status(401);
+            res.body.should.be.an('object');
+
+            should.exist(res.body.message);
         });
 
-        it('it should return 401 (invalid authorization format)', (done) => {
-            chai.request(server)
-                .post('/ticket')
-                .send({
-                    title: 'Ticket test',
-                    description: 'Ticket description'
-                })
-                .set('Authorization', 'test@testdomain.com')
-                .end((err, res) => {
-                    res.should.have.status(401);
-                    res.body.should.be.an('object');
-
-                    should.exist(res.body.message);
-                    done();
-                });
-        });
-
-        it('it should create a ticket', (done) => {
-            chai.request(server)
+        it('it should create a ticket', async () => {
+            const res = await chai.request(server)
                 .post('/ticket')
                 .set('Authorization', 'Bearer test@testdomain.com')
                 .send({
                     title: 'Ticket test',
                     description: 'Ticket description'
-                })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.an('object');
-
-                    res.body.title.should.eql('Ticket test');
-                    res.body.description.should.eql('Ticket description');
-                    res.body.status.should.eql('todo');
-                    done();
                 });
+
+            res.should.have.status(200);
+            res.body.should.be.an('object');
+
+            res.body.title.should.eql('Ticket test');
+            res.body.description.should.eql('Ticket description');
+            res.body.status.should.eql('todo');
         });
 
-        it('it should fail to create a ticket (empty body)', (done) => {
-            chai.request(server)
+        it('it should fail to create a ticket (empty body)', async () => {
+            const res = await chai.request(server)
                 .post('/ticket')
                 .set('Authorization', 'Bearer test@testdomain.com')
-                .send({})
-                .end((err, res) => {
-                    res.should.have.status(500);
-                    res.body.should.be.an('object');
+                .send({});
 
-                    should.exist(res.body.message);
-                    done();
-                });
+            res.should.have.status(500);
+            res.body.should.be.an('object');
+
+            should.exist(res.body.message);
         });
     });
 
     describe('PUT /ticket/:id', () => {
-        let ticketFromTest2 = null;
-
-        before(async () => {
+        it('it should fail to update others ticket (401)', async () => {
             const user = await UserRepo.save({
                 username: 'test2',
                 mail: 'test2@testdomain.com'
             });
 
-            ticketFromTest2 = await TicketRepo.save({
+            const ticketFromTest2 = await TicketRepo.save({
                 user,
                 title: 'test',
                 description: 'test'
             });
-        });
 
-        it('it should fail to update others ticket (401)', async () => {
             const res = await chai.request(server)
                 .put(`/ticket/${ticketFromTest2.id}`)
                 .set('Authorization', 'Bearer test@testdomain.com')
@@ -211,6 +263,10 @@ describe('Ticket', () => {
         });
     });
 
-    // finally empty user table
-    after(() => UserRepo.delete({}));
+    // finally empty everything
+    after(async () => {
+        await CommentRepo.delete({});
+        await TicketRepo.delete({});
+        await UserRepo.delete({});
+    });
 });
